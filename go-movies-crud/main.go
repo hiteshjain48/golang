de7a41259eb1b43a309921335bce47ab43b5d92a
Hitesh Jain <hiteshjainhd@gmail.com>
2023-05-06 23:30:12 +0530

    declared endpoints

---
+++ go-movies-crud/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
+	"encoding/json"
+)
+
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName string `json:"lastname"`
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+	fmt.Println("server on 8000")
+	log.Fatal(http.ListenAndServe(":8000",r))
+}
