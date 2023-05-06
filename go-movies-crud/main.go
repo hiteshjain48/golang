004bdc232e70cc93d0e370789e904c91d0e58936
Hitesh Jain <hiteshjainhd@gmail.com>
2023-05-07 00:04:54 +0530

    declared getMovies, deleteMovie and getMovie endpoints

--- go-movies-crud/main.go
+++ go-movies-crud/main.go
@@ -21,9 +21,43 @@ type Director struct {
 	LastName string `json:"lastname"`
 }
 
+var movies = []Movie{}
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]... )
+			break
+		}
+	}
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range movies {
+
+		if item.ID == params["id"] {
+
+			json.NewEncoder(w).Encode(item)
+			break
+		}
+	}
+}
 func main() {
 	r := mux.NewRouter()
 
+	movies = append(movies, Movie{ID:"1", Isbn: "443233", Title: "Movie One",Director: &Director{FirstName: "John", LastName: "Doe"}})
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
