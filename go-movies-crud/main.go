9ae914737761c753c1514e3118145b1a0e81eea6
Hitesh Jain <hiteshjainhd@gmail.com>
2023-05-07 21:22:54 +0530

    done testing

--- go-movies-crud/main.go
+++ go-movies-crud/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
-	"github.com/gorilla/mux"
 	"log"
 	"math/rand"
-	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Movie struct {
@@ -51,6 +53,35 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 
 			json.NewEncoder(w).Encode(item)
 			break
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	var movie Movie
+	
+	_= json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]... )
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
 		}
 	}
 }
