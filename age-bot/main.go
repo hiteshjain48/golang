ef44f73fc52c0cc6b3165a8debfc473254d3bc80
Hitesh Jain <hiteshjainhd@gmail.com>
2023-05-13 23:18:20 +0530

    added func

--- age-bot/main.go
+++ age-bot/main.go
@@ -9,6 +9,9 @@ import (
 	"github.com/shomali11/slacker"
 )
 
+func printCommandEvents(bot.CommandEvents()) {
+
+}
 func main() {
 	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5242248788342-5273380460768-uKEbPuUhx2qqW87Fj1EJtmxc")
 	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A057PG5MAQZ-5242321498006-a944f7bc624cfc262d7da94205042c2b7e273f8b747a66d6ed7f15a0ac094030")
